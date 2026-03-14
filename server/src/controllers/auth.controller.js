449dcf949c123e523cff5125d4f835f9df4c14b4
hamzakargin <hamzakargin@outlook.com>
2026-03-14 14:01:24 +0400

    feat(auth): implement user signup with validation, hashing and token generation

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "Tüm alanlar zorunludur" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Şifre en az 6 karakter olmalıdır" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Geçersiz e-posta formatı" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user)
+      return res.status(400).json({ message: "Bu e-posta zaten kullanılıyor" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Geçersiz kullanıcı verisi" });
+    }
+  } catch (error) {
+    console.log("Signup controller hatası:", error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+};
