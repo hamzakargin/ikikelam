e5954a40a457b1bf57034b4f5f6fbe4ad10ddd34
hamzakargin <hamzakargin@outlook.com>
2026-04-30 10:38:21 +0400

    client side setup

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -7,40 +7,32 @@ import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
-
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "Tüm alanlar zorunludur" });
     }
-
     if (password.length < 6) {
       return res
         .status(400)
         .json({ message: "Şifre en az 6 karakter olmalıdır" });
     }
-
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ message: "Geçersiz e-posta formatı" });
     }
-
     const user = await User.findOne({ email });
     if (user)
       return res.status(400).json({ message: "Bu e-posta zaten kullanılıyor" });
-
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-
     const newUser = new User({
       fullName,
       email,
       password: hashedPassword,
     });
-
     if (newUser) {
       const savedUser = await newUser.save();
       generateToken(savedUser._id, res);
-
       res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
