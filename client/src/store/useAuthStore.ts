7cf6b55222c6ebf1ace282b42f58baf91a002b76
hamzakargin <hamzakargin@outlook.com>
2026-04-30 12:02:54 +0400

    signup page added

--- client/src/store/useAuthStore.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useAuthStore = create((set) => ({
-  authUser: { name: "Namik", _id: 123, age: 25 },
-  isLoading: false,
-  login: () => {
-    console.log("logged");
-  },
-}));
--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,15 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { rateLimiter } from "./middleware/rateLimiter.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(rateLimiter);
 app.use("/api/auth", authRoutes);
